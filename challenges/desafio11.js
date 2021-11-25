8553f8f94231474a1856fc2a8fd2f90da36782cc
Ivan dos Santos Silva <ivan@pop-os.localdomain>
2021-11-24 21:48:37 -0300

    Correcao Requisito 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,13 +1,12 @@
-db.produtos.updateMany(
-  { 
-    $push: {
-      tags: {
-        $each: ["combo", "tasty"],
-        $sort: 1,
-      },
+db.produtos.updateMany({},
+{ 
+  $push: {
+    tags: {
+      $each: ["combo", "tasty"],
+      $sort: 1,
     },
-  }, 
-);
+  },
+});
 
 db.produtos.find(
   {},
